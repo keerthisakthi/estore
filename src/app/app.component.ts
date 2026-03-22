d6174692a5ecd56465aafd132dd6fb39f771c3f6
keerthisakthi <keerthisakthi017@outlook.com>
2026-03-22 17:29:22 +0530

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent],
 })
 export class AppComponent {
   title = 'estore';
